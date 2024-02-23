d2ff52a48a98b90440cd9066529fecc501ca7484
Nikolai Poriadin <np25071984@gmail.com>
2024-02-23 07:51:17 -0500

    Add Repositories

---
+++ source/src/Repository/RecordRepository.php
@@ -0,0 +1,133 @@
+<?php declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\RecordEntity;
+use App\Entity\UserEntity;
+use Doctrine\DBAL\Connection;
+use App\State\StateFactory;
+use DateTimeImmutable;
+use DateInterval;
+
+class RecordRepository
+{
+    public function __construct(
+        private Connection $connection,
+        private StateFactory $stateFactory,
+    ) {}
+
+    public function getRandomRecord(int $userId): RecordEntity
+    {
+        $query = <<<SQL
+            SELECT
+                d."key",
+                d.record_id,
+                CASE WHEN ud.meaning IS NULL THEN d.meaning ELSE ud.meaning END AS meaning,
+                CASE WHEN ud.links IS NULL THEN d.links ELSE ud.links END AS links,
+                state
+            FROM "user-dictionary" ud
+            INNER JOIN dictionary d ON d.record_id = ud.record_id
+            WHERE ud.user_id = {$userId}
+            ORDER BY RANDOM()
+            LIMIT 1
+            SQL;
+
+        $records = $this->connection->fetchAllAssociative($query);
+        $rawRecord = $records[0];
+        if (is_null($rawRecord)) {
+            throw new \Exception("Record wasn't found");
+        }
+
+        return $this->buildEntity($userId, $rawRecord);
+    }
+
+    public function findRecord(int $userId): ?RecordEntity
+    {
+        $query = <<<SQL
+            SELECT
+                d."key",
+                d.record_id,
+                CASE WHEN ud.meaning IS NULL THEN d.meaning ELSE ud.meaning END AS meaning,
+                CASE WHEN ud.links IS NULL THEN d.links ELSE ud.links END AS links,
+                state
+            FROM "user-dictionary" ud
+            INNER JOIN dictionary d ON d.record_id = ud.record_id
+            WHERE ud.user_id = {$userId} AND due < NOW()
+            ORDER BY due ASC
+            LIMIT 1
+            SQL;
+
+        $records = $this->connection->fetchAllAssociative($query);
+        $rawRecord = $records[0] ?? null;
+        if (is_null($rawRecord)) {
+            return null;
+        }
+
+        return $this->buildEntity($userId, $rawRecord);
+    }
+
+    public function getByUserAndRecordId(int $userId, int $recordId): RecordEntity
+    {
+        $query = <<<SQL
+            SELECT
+                d."key",
+                d.record_id,
+                CASE WHEN ud.meaning IS NULL THEN d.meaning ELSE ud.meaning END AS meaning,
+                CASE WHEN ud.links IS NULL THEN d.links ELSE ud.links END AS links,
+                state
+            FROM "user-dictionary" ud
+            INNER JOIN dictionary d ON d.record_id = ud.record_id
+            WHERE ud.user_id = {$userId} AND ud.record_id = {$recordId}
+            SQL;
+
+        $records = $this->connection->fetchAllAssociative($query);
+        $rawRecord = $records[0];
+        if (is_null($rawRecord)) {
+            throw new \Exception("Record wasn't found");
+        }
+
+        return $this->buildEntity($userId, $rawRecord);
+    }
+
+    public function updateState(UserEntity $user, RecordEntity $record): void
+    {
+        $delayDays = $record->getState()->getDelay();
+        $timeZone = $user->getTimezone();
+        $today = new DateTimeImmutable('NOW', new \DateTimeZone($timeZone));
+        // Show Record after 6:00 am user local time
+        $due = $today->add(new DateInterval("P{$delayDays}D"))->setTime(6, 0);
+        $dueString = $due->format('Y-m-d H:i:sP');
+
+        $query = <<<SQL
+            UPDATE "user-dictionary" ud
+                SET "state" = ?,
+                due = ?
+            WHERE user_id = ? AND record_id = ?
+            SQL;
+
+        $this->connection->executeStatement(
+            $query,
+            [
+                $record->getState()->getId(),
+                $dueString,
+                $record->getUserId(),
+                $record->getRecordId()
+            ]
+        );
+    }
+
+    private function buildEntity(int $userId, array $rawData): RecordEntity
+    {
+        $links = json_decode($rawData["links"] ?? "[]", true);
+        $state = $this->stateFactory->getState($rawData["state"]);
+
+        return new RecordEntity(
+            (int)$rawData["record_id"],
+            $userId,
+            $state,
+            $rawData["key"],
+            $rawData["meaning"],
+            $links
+        );
+    }
+}
---
+++ source/src/Repository/UserRepository.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\UserEntity;
+use Doctrine\DBAL\Connection;
+
+class UserRepository
+{
+    public function __construct(
+        private Connection $connection
+    ) {}
+
+    public function getUserByEmail(string $email): UserEntity
+    {
+        $users = $this->connection->fetchAllAssociative(sprintf("SELECT * FROM \"user\" WHERE email='%s'", $email));
+        $rawUser = $users[0] ?? null;
+        if (is_null($rawUser)) {
+            throw new \Exception("User wasn't found");
+        }
+
+        return $this->buildEntity($rawUser);
+    }
+
+    public function getUserById(int $id): UserEntity
+    {
+        $users = $this->connection->fetchAllAssociative(sprintf("SELECT * FROM \"user\" WHERE id=%d", $id));
+        // $users = $this->connection->fetchAllAssociative("SELECT * FROM \"user\"");
+        $rawUser = $users[0] ?? null;
+        if (is_null($rawUser)) {
+            throw new \Exception("User wasn't found");
+        }
+
+        return $this->buildEntity($rawUser);
+    }
+
+    private function buildEntity(array $rawData): UserEntity
+    {
+        return new UserEntity(
+            (int)$rawData["id"],
+            $rawData["email"],
+            $rawData["firstname"],
+            $rawData["lastname"],
+            $rawData["timezone"]
+        );
+    }
+}
