26f3052d5cd3aadac91eceed91a852b7c27dffc4
Nikolai Poriadin <np25071984@gmail.com>
2024-02-23 07:51:39 -0500

    RecordController

--- source/src/Controller/RecordController.php
+++ source/src/Controller/RecordController.php
@@ -1,53 +1,56 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Entity\RecordEntity;
+use App\Repository\RecordRepository;
+use App\Repository\UserRepository;
 
 class RecordController extends AbstractController
 {
     function __construct(
-        private Connection $connection
+        private UserRepository $userRepository,
+        private RecordRepository $recordRepository,
     ) {}
 
-    #[Route('/')]
-    public function getRecord(): Response
+    #[Route('/{userId}', name: 'show_record', methods: ['GET'])]
+    public function showRecord(int $userId): Response
     {
-        // TODO: implement Repository/DataMapper design pattern
-        $users = $this->connection->fetchAllAssociative("SELECT * FROM \"user\" WHERE email='mail@mail.com'");
-        $user = $users[0] ?? null;
-        if (is_null($user)) {
-            throw new \Exception("User wasn't found");
+        $user = $this->userRepository->getUserById($userId);
+        $record = $this->recordRepository->findRecord($user->getId());
+        if (is_null($record)) {
+            return $this->render('no-records.html.twig', [
+                'user' => $user,
+            ]);
         }
-        $userName = $user["email"] ?? "<unknown>";
-        $userId = (int)$user["id"];
-
-        $query = <<<SQL
-            SELECT
-                d."key",
-                d.record_id,
-                CASE WHEN ud.meaning IS NULL THEN d.meaning ELSE ud.meaning END AS meaning,
-                CASE WHEN ud.links IS NULL THEN d.links ELSE ud.links END AS links,
-                state
-            FROM "user-dictionary" ud
-            INNER JOIN dictionary d ON d.record_id = ud.record_id
-            WHERE ud.user_id = {$userId}
-            ORDER BY RANDOM()
-            LIMIT 1
-            SQL;
-
-        $records = $this->connection->fetchAllAssociative($query);
-        $rawRecord = $records[0];
-        $links = json_decode($rawRecord["links"] ?? "[]", true);
-        $record = new RecordEntity($userId, (int)$rawRecord["record_id"], $rawRecord["key"], $rawRecord["meaning"], $links);
 
         return $this->render('index.html.twig', [
-            'username' => $userName,
+            'user' => $user,
             "record" => $record,
         ]);
+    }
+
+    #[Route('/{userId}/state/{recordId}/forward', name: 'move_record_forward', methods: ['GET'])]
+    public function moveForward(int $userId, int $recordId)
+    {
+        $user = $this->userRepository->getUserById($userId);
+        $record = $this->recordRepository->getByUserAndRecordId($userId, $recordId);
+        $record->getState()->next();
+        $this->recordRepository->updateState($user, $record);
+
+        return $this->redirectToRoute("show_record", ['userId' => $userId]);
+    }
+
+    #[Route('/{userId}/state/{recordId}/backward', name: 'move_record_backward', methods: ['GET'])]
+    public function moveBackward(int $userId, int $recordId)
+    {
+        $user = $this->userRepository->getUserById($userId);
+        $record = $this->recordRepository->getByUserAndRecordId($userId, $recordId);
+        $record->getState()->rollback();
+        $this->recordRepository->updateState($user, $record);
+
+        return $this->redirectToRoute("show_record", ['userId' => $userId]);
     }
 }
