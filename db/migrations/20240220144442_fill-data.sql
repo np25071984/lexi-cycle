e390267bd344cd17d81465674674bf86a073f874
Nikolai Poriadin <np25071984@gmail.com>
2024-02-27 07:42:08 -0500

    More data in migration

--- db/migrations/20240220144442_fill-data.sql
+++ db/migrations/20240220144442_fill-data.sql
@@ -75,40 +75,119 @@ If someone reveals a particularly unique, smart, and original idea, you might sa
     '[{"title": "ProWritingAid", "url": "https://prowritingaid.com/genius-vs-genious"}]'
 );
 
+INSERT INTO dictionary("key", meaning, links) VALUES ('complement',
+    'To make something else seem better or more attractive when combining with it.
+
+* Strawberries and cream complement each other perfectly.
+
+* The music complements her voice perfectly.',
+    '[{"title": "Cambridge Dictionary", "url": "https://dictionary.cambridge.org/us/dictionary/english/complement"}]'
+);
+
+INSERT INTO dictionary("key", meaning, links) VALUES ('how come',
+    'Used to ask about the reason for something.
+
+* So how come you got an invitation and not me?',
+    '[{"title": "Cambridge Dictionary", "url": "https://dictionary.cambridge.org/us/dictionary/english/how-come"}]'
+);
+
+INSERT INTO dictionary("key", meaning, links) VALUES ('beside',
+    'At the side of, next to.
+
+* Come and sit here beside me.
+
+* Our school was built right beside a river.',
+    '[{"title": "Cambridge Dictionary", "url": "https://dictionary.cambridge.org/us/dictionary/english/beside"}, {"title": "YouGlish", "url": "https://youglish.com/pronounce/beside/english?"}]'
+);
+
+INSERT INTO dictionary("key", meaning, links) VALUES ('besides',
+    'In addition to; also.
+
+* Do you play any other sports besides basketball?
+
+* The author''s wife was a good editor, besides being a great writer herself.',
+    '[{"title": "Cambridge Dictionary", "url": "https://dictionary.cambridge.org/us/dictionary/english/besides"}, {"title": "YouGlish", "url": "https://youglish.com/pronounce/besides/english?"}]'
+);
+
+INSERT INTO dictionary("key", meaning, links) VALUES ('adore',
+    'to love someone very much, especially in a way that shows a lot of admiration or respect, or to like something very much.
+
+* She has one son and she adores him.
+
+* I absolutely adore chocolate.',
+    '[{"title": "Cambridge Dictionary", "url": "https://dictionary.cambridge.org/us/dictionary/english/adore"}]'
+);
+
+INSERT INTO dictionary("key", meaning, links) VALUES ('badass',
+    'A bad or slightly frightening person.
+
+* He plays a 21-year old badass who gets into a lot of trouble.',
+    '[{"title": "Cambridge Dictionary", "url": "https://dictionary.cambridge.org/us/dictionary/english/badass"}, {"title": "YouGlish", "url": "https://youglish.com/pronounce/badass/english?"}]'
+);
+
+INSERT INTO dictionary("key", meaning, links) VALUES ('predominantly',
+    'Mostly or mainly.
+
+* A predominantly Muslim community.
+
+* She is predominantly a dancer, but she also sings.',
+    '[{"title": "Cambridge Dictionary", "url": "https://dictionary.cambridge.org/us/dictionary/english/predominantly"}]'
+);
+
+INSERT INTO dictionary("key", meaning, links) VALUES ('in limbo',
+    'In a situation where you do not know what will happen or when something will happen.
+
+* We were in limbo for weeks while the jury tried to make a decision in the case.',
+    '[{"title": "Cambridge Dictionary", "url": "https://dictionary.cambridge.org/us/dictionary/english/in-limbo"}]'
+);
+
+INSERT INTO dictionary("key", meaning, links) VALUES ('mind-boggling',
+    'Hard to comprehend; confusing.
+
+* The amount of paperwork involved is mind-boggling.',
+    '[{"title": "Collins", "url": "https://www.collinsdictionary.com/us/dictionary/english/mind-boggling"}]'
+);
+
+INSERT INTO dictionary("key", meaning, links) VALUES ('slam dunk',
+    'If you say that something is a **slam dunk**, you mean that a success or victory will be easily achieved.
+
+* So it''s an easy decision. It''s a slam dunk.
+
+In basketball, a **slam dunk** is a shot in which a player jumps up and forces the ball through the basket.
+
+* ...a series of spectacular slam dunks.',
+    '[{"title": "Collins", "url": "https://www.collinsdictionary.com/us/dictionary/english/slam-dunk"}]'
+);
+
+INSERT INTO dictionary("key", meaning, links) VALUES ('eat, sleep, work, repeat',
+    'This is a lazy man''s lifestyle.',
+    '[{"title": "Quora", "url": "https://www.quora.com/What-is-the-meaning-of-the-phrase-Eat-Sleep-Repeat"}]'
+);
+
+INSERT INTO dictionary("key", links) VALUES ('would you be interested in ...?',
+    '[{"title": "YouTube", "url": "https://youtu.be/oIlGC71duBU?si=qydtRqmdbw1g1XdE&t=20"}]'
+);
+
+INSERT INTO dictionary("key", links) VALUES ('how do you handle ...?',
+    '[{"title": "YouTube", "url": "https://youtu.be/oIlGC71duBU?si=4CLg6b5Qyc29GCDQ&t=38"}]'
+);
+
+INSERT INTO dictionary("key", links) VALUES ('are you planning to ...?',
+    '[{"title": "YouTube", "url": "https://youtu.be/oIlGC71duBU?si=dAXNFcvZ8utewg3D&t=64"}]'
+);
+
+INSERT INTO dictionary("key") VALUES ('organize my thoughts');
+
 INSERT INTO dictionary("key") VALUES ('Rome was not built overnight');
 
 INSERT INTO "user"(email, password, firstname, lastname, timezone)
 VALUES ('mail@mail.com', 'secret', 'Lexi', 'Cycle', 'US/Eastern');
 
-INSERT INTO "user-dictionary"(user_id, record_id, "state") VALUES (
-    (SELECT id FROM "user" WHERE email = 'mail@mail.com'),
-    (SELECT record_id FROM "dictionary" WHERE "key" = 'mesmerizing'),
-    'state_90'
-);
+INSERT INTO "user-dictionary"(user_id, record_id, "state")
+SELECT (SELECT id FROM "user" WHERE email = 'mail@mail.com'), record_id FROM dictionary;
 
-INSERT INTO "user-dictionary"(user_id, record_id, "state") VALUES (
-    (SELECT id FROM "user" WHERE email = 'mail@mail.com'),
-    (SELECT record_id FROM "dictionary" WHERE "key" = 'come across'),
-    'state_30'
-);
-
-INSERT INTO "user-dictionary"(user_id, record_id, "state") VALUES (
-    (SELECT id FROM "user" WHERE email = 'mail@mail.com'),
-    (SELECT record_id FROM "dictionary" WHERE "key" = 'stray <noun>'),
-    'state_7'
-);
-
-INSERT INTO "user-dictionary"(user_id, record_id, "state") VALUES (
-    (SELECT id FROM "user" WHERE email = 'mail@mail.com'),
-    (SELECT record_id FROM "dictionary" WHERE "key" = 'shoddy'),
-    'state_1'
-);
-
-INSERT INTO "user-dictionary"(user_id, record_id, "state") VALUES (
-    (SELECT id FROM "user" WHERE email = 'mail@mail.com'),
-    (SELECT record_id FROM "dictionary" WHERE "key" = 'Rome was not built overnight'),
-    'state_0'
-);
+INSERT INTO "user-dictionary"(user_id, record_id, state)
+SELECT (SELECT id FROM "user" WHERE email = 'mail@mail.com'), record_id, 'state_0' FROM dictionary;
 
 -- migrate:down
 DELETE FROM "user-dictionary";
